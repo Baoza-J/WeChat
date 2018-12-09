92e0f4cb0ea9ac9326da632fb4405c06f11b4c76
Baoza-J <guantangbaoza@163.com>
2018-12-09 12:47:01 +0800

    python3中的input 函数

---
+++ python3中的input函数/运算.py
@@ -0,0 +1,52 @@
+def get():
+    a = input('请输入第一个数:\n')
+    c = input('请输入运算符+、-、*、/：\n')
+    b = input('请输入第二个数:\n')
+
+
+    if c is '+':
+        try:
+            a = float(a)
+            b = float(b)
+            print('结果为', a+b)
+            print()
+        except (ValueError, TypeError):
+            print('你输入的有问题\n')
+
+    elif c is '-':
+        try:
+            a = float(a)
+            b = float(b)
+            print('结果为', a-b)
+            print()
+        except (ValueError, TypeError):
+            print('你输入的有问题\n')
+
+    elif c is '*':
+        try:
+            a = float(a)
+            b = float(b)
+            print('结果为', a*b)
+            print()
+        except (ValueError, TypeError):
+            print('你输入的有问题\n')
+
+    elif c is '/':
+        try:
+            a = float(a)
+            b = float(b)
+            print('结果为', a/b)
+            print()
+        except (ValueError, ZeroDivisionError, TypeError):
+            print('被除数不能是零！！！\n')
+
+if __name__ == '__main__':
+    while True:
+        d = input("准备运算，按'y'继续，按'q'退出\n")
+        if d == 'y':
+            get()
+        elif d == 'q':
+            print('吃饭吧，别算了。')
+            break
+        else:
+            print('输入的不对，从新来\n')
