aa57ae0448f9061347a1139a6a57d21fbc3b31f1
Baoza-J <guantangbaoza@163.com>
2018-12-10 20:15:43 +0800

    改变了try except 结构，细化了判断错误类型

--- python3中的input函数/运算.py
+++ python3中的input函数/运算.py
@@ -1,48 +1,40 @@
 def get():
-    a = input('请输入第一个数:\n')
-    c = input('请输入运算符+、-、*、/：\n')
-    b = input('请输入第二个数:\n')
+    try:
+        a = input('请输入第一个数:\n')
+        a = float(a)
 
+        c = input('请输入运算符+、-、*、/：\n')
+        if c not in ['+', '-', '*', '/']:
+            'df' * 'df'
+            
+        b = input('请输入第二个数:\n')
+        b = float(b)
 
-    if c is '+':
-        try:
-            a = float(a)
-            b = float(b)
+        if c is '+':
             print('结果为', a+b)
             print()
-        except (ValueError, TypeError):
-            print('你输入的有问题\n')
 
-    elif c is '-':
-        try:
-            a = float(a)
-            b = float(b)
+        elif c is '-':
             print('结果为', a-b)
             print()
-        except (ValueError, TypeError):
-            print('你输入的有问题\n')
 
-    elif c is '*':
-        try:
-            a = float(a)
-            b = float(b)
+        elif c is '*':
             print('结果为', a*b)
             print()
-        except (ValueError, TypeError):
-            print('你输入的有问题\n')
 
-    elif c is '/':
-        try:
-            a = float(a)
-            b = float(b)
+        elif c is '/':
             print('结果为', a/b)
             print()
-        except (ValueError, ZeroDivisionError, TypeError):
-            print('你输入的有问题！！！\n')
 
-    elif c not in ['+','-','*','/']:
-        print('你输入的有问题\n')
+    except ZeroDivisionError:
+        print('被除数不能是零\n')
 
+    except ValueError:
+        print('你输入的值有问题\n')
+
+    except TypeError:
+        print('你输入的运算符有问题\n')
+            
 if __name__ == '__main__':
     while True:
         d = input("准备运算，按'y'继续，按'q'退出\n")
